b93aa3afb03aeab5aa19ee5c6909d400a868cd0a
bilel <bilelh532@gmail.com>
2019-06-18 00:35:05 +0200

    Modifier le DOM almost DONE

--- 02-DOM/02-events/events.js
+++ 02-DOM/02-events/events.js
@@ -1,19 +1,40 @@
 var par = document.getElementById('par');
+
+var title1 = document.getElementById('title-1');
+
+title1.onclick = function() {
+    console.log('click')
+    var p = document.createElement("p");
+    var newpara = document.createTextNode('new paragraphe');
+    document.body.appendChild(p);
+    
+    p.classList.add('text-red');
+    p.id='par'
+    p.appendChild(newpara);
+
+}
+
 par.addEventListener('mouseover', function(event) { 
-    event.target.style.color = "green";
     var width = window.innerWidth;
     var x = event.x;
     var y = event.y;
     console.log('x : ' + x + '   y : ' + y);
     if(x < width / 2){
         console.log('GAUCHE');
+        this.classList.replace('text-red' , 'text-black');
+
     } else {
         console.log('DROITE');
+        this.classList.replace('text-black' , 'text-red');
+
     }
 })
 
 par.addEventListener('mouseout', function(event) {
-    event.target.style.color = "red";
+    console.log('OUT');
+        this.classList.replace('text-black' , 'text-red');
 })
+
+
 
 
